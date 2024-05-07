67a63a6aa3288face89982ecb3ffdd1dba6ec554
Auriel-Wish <44419323+Auriel-Wish@users.noreply.github.com>
2024-05-08 10:56:31 +1200

    Web interface now allows to run argument through both the premade model and the model trained on the IBM data

--- Part 2/backend.py
+++ Part 2/backend.py
@@ -4,7 +4,7 @@
 # backend.py
 # ----------------------------------------------------------------
 from flask import Flask, request, jsonify, render_template
-from funcs import eval_argument
+from funcs import eval_argument_premade, eval_argument_IBM
 
 app = Flask(__name__)
 
@@ -13,11 +13,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-# Get the argument and return its evaluation
-@app.route("/get_analysis", methods=["POST"])
-def get_analysis():
+# Get the argument and return its evaluation (model 1)
+@app.route("/get_analysis_premade", methods=["POST"])
+def get_analysis_premade():
     user_argument = request.form["user_argument"]
-    return jsonify(eval_argument(user_argument))
+    return jsonify(eval_argument_premade(user_argument))
+
+# Get the argument and return its evaluation (model 2)
+@app.route("/get_analysis_IBM", methods=["POST"])
+def get_analysis_IBM():
+    user_argument = request.form["user_argument"]
+    return jsonify(eval_argument_IBM(user_argument))
 
 # Run application
 if __name__ == "__main__":
--- Part 2/funcs.py
+++ Part 2/funcs.py
@@ -5,7 +5,7 @@
 # ----------------------------------------------------------------
 from transformers import pipeline
 
-def eval_argument(arg):
+def eval_argument_premade(arg):
     pipe = pipeline("text-classification", model="chkla/roberta-argument", return_all_scores=True)
 
     # run argument through the model
@@ -15,6 +15,17 @@ def eval_argument(arg):
         if res['label'] == 'ARGUMENT':
             arg_val = res['score']
             break
+
+    if arg_val < 0.01:
+        arg_val = 0
+    return (round(arg_val, 2))
+
+def eval_argument_IBM(arg):
+    pipe = pipeline("text-classification", model="/Users/aurielwish/Desktop/Trial Project/Part 1/Trained-Model", return_all_scores=True)
+
+    # run argument through the model
+    result = (pipe(arg))[0][0]
+    arg_val = result['score']
 
     if arg_val < 0.01:
         arg_val = 0
