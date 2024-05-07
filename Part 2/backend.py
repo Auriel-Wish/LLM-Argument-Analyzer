9d6db5ecd5d47a3f11540a0f9877679de8114b6a
Auriel-Wish <44419323+Auriel-Wish@users.noreply.github.com>
2024-05-07 16:02:57 +1200

    Part 2 complete: User interface created, styled, and integrated with flask API

--- Part 2/backend.py
+++ Part 2/backend.py
@@ -1,24 +1,24 @@
-# flask_app.py
+# Auriel Wish
+# Trial Project: Summer 2024 Internship
+#
+# backend.py
+# ----------------------------------------------------------------
 from flask import Flask, request, jsonify, render_template
 from funcs import eval_argument
 
-# Create a Flask application
 app = Flask(__name__)
 
-# Route to serve the frontend page with the textbox
+# Initial webpage
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# Endpoint to receive text data from the frontend
-@app.route("/submit", methods=["POST"])
-def submit():
-    # Get the text data from the POST request
-    user_text = request.form["user_text"]
+# Get the argument and return its evaluation
+@app.route("/get_analysis", methods=["POST"])
+def get_analysis():
+    user_argument = request.form["user_argument"]
+    return jsonify(eval_argument(user_argument))
 
-    # Process the text data (example: simply return it back)
-    return jsonify(eval_argument(user_text))
-
-# Run the Flask app
+# Run application
 if __name__ == "__main__":
     app.run()
--- Part 2/funcs.py
+++ Part 2/funcs.py
@@ -1,5 +1,7 @@
 # Auriel Wish
 # Trial Project: Summer 2024 Internship
+#
+# funcs.py
 # ----------------------------------------------------------------
 from transformers import pipeline
 
@@ -7,6 +9,7 @@ def eval_argument(arg):
     pipe = pipeline("text-classification", model="chkla/roberta-argument")
 
     # run argument through the model
+    # invert the score if NON_ARGUMENT value is greater than ARGUMENT value
     result = pipe(arg)
     if (result[0])['label'] == 'NON-ARGUMENT':
         (result[0])['score'] = 1 - (result[0])['score']
